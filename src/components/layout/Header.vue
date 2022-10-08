c2eab9ce716a7751a0fecd7df0f8e38bef8f5f27
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2022-10-09 02:48:56 +0900

    fix: title

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,7 +2,7 @@
   <QHeader elevated class="bg-primary text-white">
     <QToolbar>
       <QToolbarTitle>
-        Vue.js | 検索条件と URL を同期させるサンプル
+        Vue.js | 検索条件を URL に反映するサンプル
       </QToolbarTitle>
     </QToolbar>
   </QHeader>
